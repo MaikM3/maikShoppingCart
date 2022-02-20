bb5112f5398554c65b046ca3b36ae54b98e64d60
MaikM3 <miguel@finalis.com>
2022-02-20 19:30:42 -0300

    local config updated

--- src/shared/conn.ts
+++ src/shared/conn.ts
@@ -8,7 +8,7 @@ const connectionString = process.env.DATABASE_URL
 
 export const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
+  ssl: (isProduction ? {
     rejectUnauthorized: false
-  }
+  } : false)
 })
